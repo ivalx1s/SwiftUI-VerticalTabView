a0fba40060fe71ff9ae7e0b4eced9d38935e8956
Ivan Oparin <oparin_ig@hotmail.com>
2023-08-30 07:41:55 +0400

    Init package

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version: 5.8
+
+import PackageDescription
+
+let package = Package(
+    name: "SwiftUI-VerticalTabView",
+    products: [
+        .library(
+            name: "SwiftUI-VerticalTabView",
+            targets: ["SwiftUI-VerticalTabView"]
+		),
+    ],
+    targets: [
+        .target(
+            name: "SwiftUI-VerticalTabView"
+		),
+    ]
+)
